f00b52e081d167d4a48e50af55b915a7db9c04f7
Carter Demars <demarscarter@yahoo.ca>
2023-09-30 11:19:05 -0400

    phase 1 skeleton

---
+++ signal_processor.py
@@ -0,0 +1,34 @@
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class SignalProcessor():
+    def __init__(self, audio):
+        self.audio = audio
+
+    def get_sampling_rate(self):
+        """
+        The function determines sampling rate in kHz of an input signal.
+        :return: sampling rate
+        """
+        return
+
+    def play_sound(self):
+        """
+        Plays the sound out loud.
+        :return: None
+        """
+        return
+
+    def plot_waveform(self):
+        """
+        Plots the sound waveform as a function of the sample number
+        :return:
+        """
+        return
+
+
+if __name__ == "__main__":
+    test_audio = None
+    processor = SignalProcessor(test_audio)
